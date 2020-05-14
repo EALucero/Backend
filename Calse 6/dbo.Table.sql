05d2c9cda69778f9355a86544ea830513f4b6ccf
EALucero <56940163+EALucero@users.noreply.github.com>
2020-05-14 12:24:28 -0300

    Delete dbo.Table.sql

--- Calse 6/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[id_Alumnos] NUMERIC NOT NULL PRIMARY KEY IDENTITY, 
-    [Nombre] NVARCHAR(50) NOT NULL, 
-    [Apellido] NVARCHAR(50) NOT NULL, 
-    [Club] NVARCHAR(50) NULL, 
-    [id_Curso] NUMERIC NOT NULL
-)
