ce4d9689c0088eed22408204ad47c8b4d0b221f5
EALucero <56940163+EALucero@users.noreply.github.com>
2020-05-14 12:37:58 -0300

    Add files via upload

---
+++ Clase 6/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[id_Alumnos] NUMERIC NOT NULL PRIMARY KEY IDENTITY, 
+    [Nombre] NVARCHAR(50) NOT NULL, 
+    [Apellido] NVARCHAR(50) NOT NULL, 
+    [Club] NVARCHAR(50) NULL, 
+    [id_Curso] NUMERIC NOT NULL
+)
